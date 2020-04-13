b5bdc4ee55356c3527e29cd1526c31cc4df2bdd9
Shuyue Yu <syu5@caltech.edu>
2020-04-13 14:39:06 -0700

    Clean up

--- visualize_predictions.py
+++ visualize_predictions.py
@@ -19,6 +19,7 @@ file_names = [f for f in file_names if '.jpg' in f]
 # save preds (overwrites any previous predictions!)
 with open(os.path.join(preds_path,'preds.json'),'r') as f:
     data = json.load(f)
+
     good_imgs = ['018','027','037','114']
     bad_imgs = ['006','012','042','185']
     for img in good_imgs:
@@ -47,3 +48,17 @@ with open(os.path.join(preds_path,'preds.json'),'r') as f:
             I[br_row-1, tl_col:br_col, :] = [0,255,0]
         img = Image.fromarray(I, 'RGB')
         img.save(os.path.join(preds_path,img_name))
+
+    # # Visualiza all the results
+    # for img_name in data:
+    #     I = Image.open(os.path.join(data_path,img_name))
+    #     I = np.array(I)
+    #     bounding_boxes = data[img_name]
+    #     for b in bounding_boxes:
+    #         [tl_row,tl_col,br_row,br_col] = b
+    #         I[tl_row:br_row, tl_col, :] = [0,255,0]
+    #         I[tl_row:br_row, br_col-1, :] = [0,255,0]
+    #         I[tl_row, tl_col:br_col, :] = [0,255,0]
+    #         I[br_row-1, tl_col:br_col, :] = [0,255,0]
+    #     img = Image.fromarray(I, 'RGB')
+    #     img.save(os.path.join(preds_path,img_name))
